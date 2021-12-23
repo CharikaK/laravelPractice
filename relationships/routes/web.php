18e63fdfe967f0e2bfbb8472d2bd51bc82b3efa4
CharixKalpi <chooti@gmail.com>
2021-12-23 16:26:54 +0000

    FinalCommit:EagerLoading

--- relationships/routes/web.php
+++ relationships/routes/web.php
@@ -16,11 +16,20 @@ use App\Models\User; // another way of creating the fake users - Not recommended
 
 Route::get('/user',function(){
 // User::factory()->count(2)->create(); // another way of creating the fake users - Not recommended
-//$users = User::all();
-$users = User::with('phone')->get();
 
+// 29 queries
+// runs below query for number of users
+// select * from `phones` where `phones`.`user_id` = 1 and `phones`.`user_id` is not null limit 1
+//$users = User::all();
+
+// Eager loading -  2 queries 
+// select * from `users`
+// select * from `phones` where `phones`.`user_id` in (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28)
+
+$users = User::with('phone')->get();
 return view('index',compact('users'));
 });
+
 
 Route::get('/phone',function(){
 // Manually inserting data into Phone
